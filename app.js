45d152816ece429d7d7a3cd9224d65ef4436573e
Forrest Almasi <forrestalmasi@gmail.com>
2014-03-20 22:37:03 -0500

    update title

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+app.use(express.favicon(__dirname + '/public/images/fav.ico'));
 
 // development only
 if ('development' == app.get('env')) {
