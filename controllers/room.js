034cb03c7cbbb36e9490740991c3c822b5812f39
Forrest Almasi <forrestalmasi@gmail.com>
2014-04-14 14:27:10 -0500

    remove local redis from room controller

--- controllers/room.js
+++ controllers/room.js
@@ -1,5 +1,10 @@
 var redis = require('redis');
-var db = redis.createClient();
+// var db = redis.createClient();
+
+
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var db = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+db.auth(redisURL.auth.split(":")[1]);
 
 module.exports = {
 	load : function(req, res) {
