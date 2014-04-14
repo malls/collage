a95f72ac572995c6b457b559dea077254a243fef
Forrest Almasi <forrestalmasi@gmail.com>
2014-04-14 14:35:10 -0500

    room

--- controllers/room.js
+++ controllers/room.js
@@ -9,9 +9,7 @@
 module.exports = {
 	load : function(req, res) {
 		// db.hgetall(req.params.room, function(err, reply) {
-			res.render('room', {
-				// imgs : reply
-			});
-		});
-	}
+			res.render('room');
+		};
+	
 };
