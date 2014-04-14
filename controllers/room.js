3fc00fd4fda4ccd2c59d24040991b28c1b4ede2b
Forrest Almasi <forrestalmasi@gmail.com>
2014-04-14 14:32:16 -0500

    room

--- controllers/room.js
+++ controllers/room.js
@@ -1,16 +1,16 @@
-var redis = require('redis');
-// var db = redis.createClient();
+// var redis = require('redis');
+// // var db = redis.createClient();
 
 
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var db = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-db.auth(redisURL.auth.split(":")[1]);
+// var redisURL = url.parse(process.env.REDISCLOUD_URL);
+// var db = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+// db.auth(redisURL.auth.split(":")[1]);
 
 module.exports = {
 	load : function(req, res) {
-		db.hgetall(req.params.room, function(err, reply) {
+		// db.hgetall(req.params.room, function(err, reply) {
 			res.render('room', {
-				imgs : reply
+				// imgs : reply
 			});
 		});
 	}
