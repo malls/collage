191645588db3a782adb33c78a3fcb68c03889849
Forrest Almasi <forrestalmasi@gmail.com>
2014-07-10 19:26:58 -0500

    room.js controller forwards db data

--- controllers/room.js
+++ controllers/room.js
@@ -1,10 +1,9 @@
 var db = require('../lib/redisConnect');
 
 module.exports = {
-	load: function(req, res) {
-		db.hgetall(req.params.room, function(err, reply) {
-      console.log(reply);
-			res.render('room', {title: req.params.room});
+	load: function (req, res) {
+		db.hgetall(req.params.room, function (err, reply) {
+			res.render('room', {title: req.params.room, data: reply});
 		});
   }
 };
