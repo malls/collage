5f1fe0c9163d9658a1f555d4fa6c6a23dabc3e45
Forrest Almasi <forrest.almasi@gmail.com>
2016-12-22 19:45:18 -0500

    update some packages

--- app.js
+++ app.js
@@ -1,81 +1,80 @@
 'use strict';
 
-var dotenv = require('dotenv');
-dotenv.load();
+const dotenv = require('dotenv').config();
 
 var port = process.env.PORT || 3000,
-  db = require('./app/rediser'),
-  garden = require('./lib/garden'),
-  express = require('express'),
-  path = require('path'),
-  app = express(),
-  index = require('./controllers/index'),
-  room = require('./controllers/room'),
-  io = require('socket.io').listen(app.listen(port));
+    db = require('./app/rediser'),
+    garden = require('./lib/garden'),
+    express = require('express'),
+    path = require('path'),
+    app = express(),
+    index = require('./controllers/index'),
+    room = require('./controllers/room'),
+    io = require('socket.io').listen(app.listen(port));
 
 // all environments
 app
-  .set('port', port)
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'jade')
-  .use(express.logger('dev'))
-  .use(express.json())
-  .use(express.urlencoded())
-  .use(express.methodOverride())
-  .use(express.multipart({
-    defer: true
-  }))
-  .use(app.router)
-  .use(express.static(path.join(__dirname, 'public'), {
-    maxAge: 42069
-  }))
-  .all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    next();
-  });
+    .set('port', port)
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'jade')
+    .use(express.logger('dev'))
+    .use(express.json())
+    .use(express.urlencoded())
+    .use(express.methodOverride())
+    .use(express.multipart({
+        defer: true
+    }))
+    .use(app.router)
+    .use(express.static(path.join(__dirname, 'public'), {
+        maxAge: 42069
+    }))
+    .all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+        next();
+    });
 
 //routes - move elsewhere
 app
-  .get('/', index.show)
-  .get('/sign', room.sign)
-  .get('/:room', room.load)
+    .get('/', index.show)
+    .get('/sign', room.sign)
+    .get('/:room', room.load)
 
 io.sockets.on('connection', function(socket) {
-  //rooms
-  socket.on('send', function(data) {
-    socket.broadcast.emit('move', data);
-  });
-
-  socket.on('savePosition', function(data) {
-    db.hset(data.room, data.id, JSON.stringify(data));
-  });
-
-  socket.on('getId', function(data) {
-    var imgid = garden.id('L');
-    socket.broadcast.emit('newimage', {
-      url: data.url,
-      id: imgid
+    //rooms
+    socket.on('send', function(data) {
+        socket.broadcast.emit('move', data);
     });
-    socket.emit('newimage', {
-      url: data.url,
-      id: imgid
-    });
-  });
 
-  socket.on('bg', function(data) {
-    socket.broadcast.emit('set', {
-      background: data.background
+    socket.on('savePosition', function(data) {
+        db.hset(data.room, data.id, JSON.stringify(data));
     });
-    db.hset(data.room, 'background', data.background);
-  });
 
-  socket.on('destroy', function(data) {
-    db.hdel(data.room, data.id);
-    socket.broadcast.emit('remove', {
-      id: data.id
+    socket.on('getId', function(data) {
+        var imgid = garden.id('L');
+        socket.broadcast.emit('newimage', {
+            url: data.url,
+            id: imgid
+        });
+        socket.emit('newimage', {
+            url: data.url,
+            id: imgid
+        });
     });
-  });
+
+    socket.on('bg', function(data) {
+        socket.broadcast.emit('set', {
+            background: data.background
+        });
+        db.hset(data.room, 'background', data.background);
+    });
+
+    socket.on('destroy', function(data) {
+        db.hdel(data.room, data.id);
+        socket.broadcast.emit('remove', {
+            id: data.id
+        });
+    });
 
 });
 
--- app/rediser.js
+++ app/rediser.js
@@ -1,33 +1,34 @@
 'use strict';
 
+const dotenv = require('dotenv').config();
+
 var url = require('url'),
     redis = require('redis'),
-    dotenv = require('dotenv'),
     db;
-
-dotenv.load();
 
 //check if it's development, then use redis db if not
 if (process.env.MODE === 'development') {
-  db = redis.createClient(process.env.REDISPORT || 6379);
+    db = redis.createClient(process.env.REDISPORT || 6379);
 } else {
-  var redisURL = url.parse(process.env.REDISCLOUD_URL);
-  db = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-  db.auth(redisURL.auth.split(":")[1]);
+    var redisURL = url.parse(process.env.REDISCLOUD_URL);
+    db = redis.createClient(redisURL.port, redisURL.hostname, {
+        no_ready_check: true
+    });
+    db.auth(redisURL.auth.split(":")[1]);
 }
 
 db.select(process.env.REDISDB || 0);
 
 //test db connection, output something in terminal
-db.set("rthdnajfondaopfda", "redis connected", function () {
-  db.get("rthdnajfondaopfda", function (err, response) {
-    console.log(response);
-  });
-  db.del("rthdnajfondaopfda");
+db.set("rthdnajfondaopfda", "redis connected", function() {
+    db.get("rthdnajfondaopfda", function(err, response) {
+        console.log(response);
+    });
+    db.del("rthdnajfondaopfda");
 });
 
-db.on("error", function (err) {
-  console.log("Redis Error: " + err);
+db.on("error", function(err) {
+    console.log("Redis Error: " + err);
 });
 
 module.exports = db;
